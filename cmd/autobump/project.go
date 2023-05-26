70259cc68b7e94c227332880534f27d8829c4f99
k4yt3x <i@k4yt3x.com>
2023-05-26 20:17:30 +0000

    completed the Git commit&push functions

--- cmd/autobump/project.go
+++ cmd/autobump/project.go
@@ -6,33 +6,75 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	log "github.com/sirupsen/logrus"
 )
 
-func processRepo(globalConfig *GlobalConfig, config *ProjectsConfig) error {
-	adapter := getAdapterByName(config.Language)
-	if adapter == nil {
-		return fmt.Errorf("invalid adapter: %s", config.Language)
-	}
-
-	projectPath := config.Path
-
-	changelogPath := filepath.Join(projectPath, "CHANGELOG.md")
-	version, err := updateChangelogFile(changelogPath)
-	if err != nil {
-		fmt.Printf("No version found in CHANGELOG.md for project at %s\n", config.Path)
-		return err
-	}
-
-	config.NewVersion = version.String()
-	err = adapter.UpdateVersion(projectPath, config)
-	if err != nil {
-		return err
-	}
-
+func openRepo(projectPath string) (*git.Repository, error) {
+	log.Infof("Opening repository at %s", projectPath)
 	repo, err := git.PlainOpen(projectPath)
+	return repo, err
+}
+
+func checkIfBranchExists(repo *git.Repository, branchName string) (bool, error) {
+	refs, err := repo.References()
+	if err != nil {
+		return false, err
+	}
+
+	branchExists := false
+	err = refs.ForEach(func(ref *plumbing.Reference) error {
+		if ref.Name().IsBranch() && ref.Name().Short() == branchName {
+			branchExists = true
+		}
+		return nil
+	})
+	return branchExists, err
+}
+
+func createAndSwitchBranch(repo *git.Repository, w *git.Worktree, branchName string, hash plumbing.Hash) error {
+	log.Infof("Creating and switching to new branch `%s`", branchName)
+	ref := plumbing.NewHashReference(plumbing.ReferenceName("refs/heads/"+branchName), hash)
+	err := repo.Storer.SetReference(ref)
+	if err != nil {
+		return err
+	}
+
+	err = w.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.ReferenceName("refs/heads/" + branchName),
+	})
+	return err
+}
+
+func commitChanges(w *git.Worktree, commitMessage string, author *object.Signature) (plumbing.Hash, error) {
+	log.Info("Committing changes")
+	commit, err := w.Commit(commitMessage, &git.CommitOptions{
+		Author: author,
+	})
+	if err != nil {
+		return plumbing.ZeroHash, err
+	}
+
+	return commit, nil
+}
+
+func pushChanges(repo *git.Repository, refSpec config.RefSpec) error {
+	log.Info("Pushing local changes to remote repository")
+	return repo.Push(&git.PushOptions{
+		RefSpecs: []config.RefSpec{refSpec},
+	})
+}
+
+func processRepo(globalConfig *GlobalConfig, projectsConfig *ProjectsConfig) error {
+	adapter := getAdapterByName(projectsConfig.Language)
+	if adapter == nil {
+		return fmt.Errorf("invalid adapter: %s", projectsConfig.Language)
+	}
+
+	projectPath := projectsConfig.Path
+	repo, err := openRepo(projectPath)
 	if err != nil {
 		return err
 	}
@@ -42,24 +84,56 @@ func processRepo(globalConfig *GlobalConfig, config *ProjectsConfig) error {
 		return err
 	}
 
-	changelogRelativePath, err := filepath.Rel(changelogPath, projectPath)
+	head, err := repo.Head()
 	if err != nil {
 		return err
 	}
 
+	branchName := "chore/bump"
+	branchExists, err := checkIfBranchExists(repo, branchName)
+	if err != nil {
+		return err
+	}
+	if branchExists {
+		return fmt.Errorf("branch %s already exists", branchName)
+	}
+
+	err = createAndSwitchBranch(repo, w, branchName, head.Hash())
+	if err != nil {
+		return err
+	}
+
+	log.Info("Updating CHANGELOG.md file")
+	changelogPath := filepath.Join(projectPath, "CHANGELOG.md")
+	version, err := updateChangelogFile(changelogPath)
+	if err != nil {
+		fmt.Printf("No version found in CHANGELOG.md for project at %s\n", projectsConfig.Path)
+		return err
+	}
+
+	projectsConfig.NewVersion = version.String()
+	err = adapter.UpdateVersion(projectPath, projectsConfig)
+	if err != nil {
+		return err
+	}
+
+	changelogRelativePath, err := filepath.Rel(projectPath, changelogPath)
+	if err != nil {
+		return err
+	}
 	result, err := w.Add(changelogRelativePath)
 	if err != nil {
 		log.Errorf("Result not expected: %v", result)
 		return err
 	}
 
-	commit, err := w.Commit("Bump version to "+config.NewVersion, &git.CommitOptions{
-		Author: &object.Signature{
-			Name:  globalConfig.GitLabConfig.UserName,
-			Email: globalConfig.GitLabConfig.Email,
-			When:  time.Now(),
-		},
-	})
+	commitMessage := "Bump version to " + projectsConfig.NewVersion
+	author := &object.Signature{
+		Name:  globalConfig.GitLabConfig.UserName,
+		Email: globalConfig.GitLabConfig.Email,
+		When:  time.Now(),
+	}
+	commit, err := commitChanges(w, commitMessage, author)
 	if err != nil {
 		return err
 	}
@@ -69,13 +143,8 @@ func processRepo(globalConfig *GlobalConfig, config *ProjectsConfig) error {
 		return err
 	}
 
-	head, err := repo.Head()
-	if err != nil {
-		return err
-	}
-
-	ref := plumbing.NewHashReference("refs/heads/chore/bump", head.Hash())
-	err = repo.Storer.SetReference(ref)
+	refSpec := config.RefSpec("refs/heads/" + branchName + ":refs/heads/" + branchName)
+	err = pushChanges(repo, refSpec)
 	if err != nil {
 		return err
 	}
@@ -99,7 +168,7 @@ func iterateProjects(globalConfig *GlobalConfig) error {
 	for _, project := range globalConfig.ProjectsConfig {
 		err := processRepo(globalConfig, &project)
 		if err != nil {
-			fmt.Printf("Error processing project at %s: %v\n", project.Path, err)
+			log.Errorf("Error processing project at %s: %v\n", project.Path, err)
 		}
 	}
 	return nil
